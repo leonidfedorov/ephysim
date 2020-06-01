d9b60908c2e1a787c0b8ea8f6cff6072ca1df4d3
Leonid Fedorov <leonidf87@gmail.com>
2020-06-01 21:54:25 +0200

    the model runs but there is a problem with parameter values; in this committhey are taken from Laing/Lord Stochastic Methods in Neuroscience chap 12

--- HH.py
+++ HH.py
@@ -10,28 +10,29 @@ t = np.linspace(0., T, n)  # Vector of times.
 
 sqrtdt = np.sqrt(dt)
 
-#TODO: biophysical model constants requires a reference to the specific neuron type)
-C_m  =   1.0 
-g_Na = 120.0 
+#ref: Hodgkin and Huxley 1952
+c_m  =   1.0
 g_K  =  36.0
-g_L  =   0.3
-E_Na =  50.0 
-E_K  = -77.0
-E_L  = -54.387
+g_Na = 120.0 
+g_l  =   0.3
+v_K  = - 12.0
+v_Na =  115.0 
+v_l  = 10.613
 
 #nonlinearities
-alpha_m = lambda V: 0.1*(V + 40.0)/(1.0 - np.exp(-(V + 40.0) / 10.0))
-beta_m = lambda V: 4.0*np.exp(-(V + 65.0) / 18.0)
-alpha_h = lambda V: 0.07*np.exp(-(V + 65.0) / 20.0)
-beta_h = lambda V: 1.0/(1.0 + np.exp(-(V + 35.0) / 10.0))
-alpha_n = lambda V: 0.01*(V + 55.0)/(1.0 - np.exp(-(V + 55.0) / 10.0))
-beta_n = lambda V: 0.125*np.exp(-(V + 65) / 80.0)
-I_Na = lambda V, m, h: g_Na * m**3 * h * (V - E_Na)
-I_K = lambda V, n: g_K  * n**4 * (V - E_K)
-I_L = lambda V: g_L * (V - E_L)
+alpha_n = lambda V: 0.01 * (10.0 - V) / (np.exp((10.0 - V) / 10.0) - 1.0)
+beta_n = lambda V: 0.125 * np.exp( (0.0 - V) / 80.0)
+alpha_m = lambda V: 0.1 * (25.0 - V) / (np.exp((25.0 - V) / 10.0) - 1.0)
+beta_m = lambda V: 4.0 * np.exp( (0.0 - V) / 18.0)
+alpha_h = lambda V: 0.07 * np.exp((0.0 - V) / 20.0)
+beta_h = lambda V: 1.0 / (np.exp((30.0 - V) / 10.0) + 1.0)
 
-#stochastic terms
-I_input = lambda mu: mu + np.random.randn()
+#additive current terms
+I_K = lambda V, n: g_K  * n**4 * (v_K - V)
+I_Na = lambda V, m, h: g_Na * m**3 * h * (v_Na - V)
+I_L = lambda V: g_l * (v_l - V)
+
+mu = 4
 
 #initial values
 V = np.zeros(n)
@@ -39,19 +40,20 @@ M = np.zeros(n)
 H = np.zeros(n)
 N = np.zeros(n)
 
-V[0] = -65
-M[0] = 0.05
-H[0] = 0.6
-N[0] = 0.32
-
+V[0] = 0.0
+N[0] = alpha_n(V[0]) / (alpha_n(V[0]) + beta_n(V[0]))
+M[0] = alpha_m(V[0]) / (alpha_m(V[0]) + beta_m(V[0]))
+H[0] = alpha_h(V[0]) / (alpha_h(V[0]) + beta_h(V[0]))
 
 
 for i in range(n - 1):
-    V[i + 1] = V[i] + dt * (0. - I_Na(V[i], M[i], H[i]) - I_K(V[i], N[i]) - I_L(V[i])) / C_m + sqrtdt * np.random.randn()
-    M[i + 1] = alpha_m(V[i])*(1.0-M[i]) - beta_m(V[i])*M[i]
-    H[i + 1] = alpha_h(V[i])*(1.0-M[i]) - beta_h(V[i])*H[i]
-    N[i + 1] = alpha_n(V[i])*(1.0-N[i]) - beta_n(V[i])*N[i]
+    V[i + 1] = V[i] + dt * (g_K * N[i] ** 4 * (v_K - V[i]) + g_Na * M[i] ** 3 * H[i] * (v_Na - V[i]) + g_l * (v_l - V[i]) + mu) / c_m #+ sqrtdt * np.random.randn()
+    N[i + 1] = alpha_n(V[i]) * (1.0 - N[i]) - beta_n(V[i]) * N[i]
+    M[i + 1] = alpha_m(V[i]) * (1.0 - M[i]) - beta_m(V[i]) * M[i]
+    H[i + 1] = alpha_h(V[i]) * (1.0 - H[i]) - beta_h(V[i]) * H[i]
+    print (i)
 
+print(range(n-1))
 fig, ax = plt.subplots(1, 1, figsize=(8, 4))
 ax.plot(t, V, lw=2)
 plt.show()
