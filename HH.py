c8245a2abf3430c4a99a9859115bb0c2ba2af548
Leonid Fedorov <leonidf87@gmail.com>
2020-06-09 17:09:11 +0200

    stochastic HH works completely - parameter set finally correct!

--- HH.py
+++ HH.py
@@ -1,59 +1,59 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#state variables
+V = []
+M = []
+H = []
+N = []
 
-#euler maruyama integration setup
-dt = .001  # Time step.
-T = 10.  # Total time.
-n = int(T / dt)  # Number of time steps.
-t = np.linspace(0., T, n)  # Vector of times.
+dt = 0.05
+t = np.linspace(0,10,10000)
 
-sqrtdt = np.sqrt(dt)
-
-#ref: Hodgkin and Huxley 1952
-c_m  =   1.0
-g_K  =  36.0
-g_Na = 120.0
-g_l  =   0.3
-v_K  = -12.0
-v_Na =  115.0 
-v_l  = 10.613
+#constants
+mu = 6.0#7 without noise
+c_m = 1.0 #microFarad
+v_Na = 50 #miliVolt
+v_K = -77  #miliVolt
+v_l = -54 #miliVolt
+g_Na = 120 #mScm-2
+g_K = 36 #mScm-2
+g_l = 0.03 #mScm-2
 
 #nonlinearities
-alpha_n = lambda V: 0.01 * (V + 55.0) / (1.0 - np.exp( - (V + 55.0) / 10.0))
-beta_n = lambda V: 0.125 * np.exp( - (V + 65.0) / 80.0)
-alpha_m = lambda V: 0.1 * (V + 40.0) / (1.0 - np.exp(-(V + 40.0) / 10.0))
-beta_m = lambda V: 4.0 * np.exp( - (V + 65.0) / 18.0)
-alpha_h = lambda V: 0.07 * np.exp( - (V + 65.0) / 20.0)
-beta_h = lambda V: 1.0 / (1.0 + np.exp( - (V + 35.0) / 10.0))
+alpha_N = lambda v: 0.01 * (v + 50)/(1 - np.exp(- (v + 50) / 10))
+beta_N = lambda v: 0.125 * np.exp(- (v + 60) / 80)
+alpha_M = lambda v: 0.1 * (v + 35)/(1 - np.exp(- (v + 35) / 10))
+beta_M = lambda v: 4.0 * np.exp(- 0.0556 * (v + 60))
+alpha_H = lambda v: 0.07 * np.exp(- 0.05 * (v + 60))
+beta_H = lambda v: 1 / (1 + np.exp(- (0.1) * (v + 30)))
 
-#additive current terms
-I_K = lambda V, n: g_K  * n**4 * (v_K - V)
-I_Na = lambda V, m, h: g_Na * m**3 * h * (v_Na - V)
-I_L = lambda V: g_l * (v_l - V)
+#intitial conditions:
+V.append(-60)
+M.append(alpha_M(V[0]) / (alpha_M(V[0]) + beta_M(V[0])))
+N.append(alpha_N(V[0]) / (alpha_N(V[0]) + beta_N(V[0])))
+H.append(alpha_H(V[0]) / (alpha_H(V[0]) + beta_H(V[0])))
 
-mu = -20#0.0
+sqrtdt = np.sqrt(dt) #Euler-Maruyama
 
-#initial values
-V = np.zeros(n)
-M = np.zeros(n)
-H = np.zeros(n)
-N = np.zeros(n)
+#electrode = []
+#electrode.append(30-V[0])
 
-V[0] = -65.0
-N[0] = alpha_n(V[0]) / (alpha_n(V[0]) + beta_n(V[0]))
-M[0] = alpha_m(V[0]) / (alpha_m(V[0]) + beta_m(V[0]))
-H[0] = alpha_h(V[0]) / (alpha_h(V[0]) + beta_h(V[0]))
+for i in range(1,len(t)):
+    M.append(M[i-1] + dt * ((alpha_M(V[i-1]) * (1 - M[i-1])) - beta_M(V[i-1]) * M[i-1]))
+    N.append(N[i-1] + dt * ((alpha_N(V[i-1]) * (1 - N[i-1])) - beta_N(V[i-1]) * N[i-1]))
+    H.append(H[i-1] + dt * ((alpha_H(V[i-1]) * (1 - H[i-1])) - beta_H(V[i-1]) * H[i-1]))
+
+    #additive current terms
+    I_Na = g_Na * H[i-1] * (M[i-1]) ** 3 * (V[i-1] - v_Na)
+    I_K = g_K * N[i-1] ** 4 * (V[i-1] - v_K)
+    I_l = g_l * (V[i-1] - v_l)    
+    I_noise = sqrtdt * np.random.randn()
+
+    V.append(V[i-1] + (dt) * ((1 / c_m) * (mu - (I_Na + I_K + I_l))) + I_noise)
+    #electrode.append((sqrtdt * np.random.randn()+30-V[i-1])/(4*np.pi))
 
 
-for i in range(n - 1):
-    V[i + 1] = V[i] - dt * (g_K * N[i] ** 4 * (V[i] - v_K) + g_Na * M[i] ** 3 * H[i] * (V[i] - v_Na) - g_l * (V[i] - v_l))/ c_m + dt * mu / c_m #+ sqrtdt * np.random.randn()
-    N[i + 1] = alpha_n(V[i]) * (1.0 - N[i]) - beta_n(V[i]) * N[i]
-    M[i + 1] = alpha_m(V[i]) * (1.0 - M[i]) - beta_m(V[i]) * M[i]
-    H[i + 1] = alpha_h(V[i]) * (1.0 - H[i]) - beta_h(V[i]) * H[i]
-
-print(range(n-1))
-fig, ax = plt.subplots(1, 1, figsize=(8, 4))
-ax.plot(t, V, lw=2)
+fig, ax = plt.subplots(1, 1, figsize=(8,4))
+ax.plot(t, electrode, lw=2)
 plt.show()
-
